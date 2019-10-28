ee029b854535b814181742908d98c6a805266f7b
Hrishikesh Thakurdesai <52523556+hmtd1992@users.noreply.github.com>
2019-10-28 21:10:11 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import pyodbc
 from bokeh.plotting import figure
 from bokeh.embed import components
 from flask import Flask, render_template
+from bokeh.server.server import Server
 app = Flask(__name__)
 
 # Index page
@@ -34,6 +35,7 @@ def index():
     
     # Embed plot into HTML via Flask Render
     script, div = components(p)
+    server.start()
     return render_template("visualization.html", script=script, div=div)
 
 
