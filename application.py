7bc21d3a9e071e7d3b829472236537fb20817f9a
Hrishikesh Thakurdesai <52523556+hmtd1992@users.noreply.github.com>
2019-10-28 12:19:39 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,12 @@ from bokeh.embed import components
 from flask import Flask, render_template
 app = Flask(__name__)
 
-def fetch_cloud_data():
+# Index page
+@app.route('/')
+def index():
+    x = []
+    y = []
+    
     server = 'visualizationserver.database.windows.net'
     database = 'visualization_data'
     username = 'hmtd1992'
@@ -21,32 +26,14 @@ def fetch_cloud_data():
         x.append(row[0])
         y.append(row[1])
         row = cursor.fetchone()
-    return x,y
-
-def create_figure(x,y):
+        
     p = figure(plot_width=600, plot_height=600,
                title='Sample Bokeh Graph',
                x_axis_label='X', y_axis_label='Y')
     p.square(x, y, size=12, color='navy', alpha=0.6)
-    return p
-
-# Index page
-@app.route('/')
-def index():
-    x = []
-    y = []
-    x,y=fetch_cloud_data()
-    my_figure = create_figure(x, y)
-
+    
     # Embed plot into HTML via Flask Render
-    script, div = components(my_figure)
+    script, div = components(p)
     return render_template("visualization.html", script=script, div=div)
-
-
-# With debug=True, Flask server will auto-reload
-# when there are code changes
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
-
 
 
